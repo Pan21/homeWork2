3ed3fc2b6c82dbffefc6907513c4e19643a8985a
Pan21 <49293503+Pan21@users.noreply.github.com>
2020-09-29 17:45:48 -0400

    Update model.js

--- model/model.js
+++ model/model.js
@@ -5,13 +5,12 @@ var MODEL = (function(){
         $.get(`../views/pages/${viewName}.html`, function(data){
             $("#content").html(data);
         });
-    };
 
-    var _getView = function(viewName){
         $.get(`../views/vids/${viewName}.html`, function(data){
             $("#vid").html(data);
         });
     };
+
 
     return {
         getView: _getView,
